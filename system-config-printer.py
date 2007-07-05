703f35773c6864a720c9b696ea499d1e02e28ead
mmcgrath <mmcgrath@8aafa1e6-caf3-4c12-8b34-7de0f4e7b767>
2007-07-05 17:42:48 +0000

    [project @ 2006-11-30 13:24:10 by twaugh] (GUI.reconnect): Provoke libcups into reconnecting.
    Original author: twaugh
    Date: 2006-11-30 13:24:10+00:00
    
    git-svn-id: svn+ssh://fedorahosted.org/svn/system-config-printer/system-config-printer@632 8aafa1e6-caf3-4c12-8b34-7de0f4e7b767

--- system-config-printer.py
+++ system-config-printer.py
@@ -586,7 +586,9 @@ class GUI:
 
     def reconnect (self):
         """Reconnect to CUPS after the server has reloaded."""
-        print "FIXME: need to reconnect to CUPS here"
+        # libcups will handle the reconnection; we just need to tell it
+        # to do something.
+        self.cups.getClasses ()
 
     def on_btnCancelConnect_clicked(self, widget):
         """Close Connect dialog"""
@@ -2750,7 +2752,7 @@ class GUI:
             return True
         self.changed = set()
         self.setDataButtonState()
-        time.sleep(0.1) # give the server a chance to process our request
+        time.sleep(1) # give the server a chance to process our request
 
         # Now reconnect, in case the server needed to reload.
         self.reconnect ()
