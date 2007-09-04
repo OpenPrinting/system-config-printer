f102c32d69f549d86c5040189ea3af823e9f55f2
twaugh <twaugh@8aafa1e6-caf3-4c12-8b34-7de0f4e7b767>
2007-09-04 09:10:19 +0000

    Updated copyright notice.
    git-svn-id: svn+ssh://fedorahosted.org/svn/system-config-printer/system-config-printer@1467 8aafa1e6-caf3-4c12-8b34-7de0f4e7b767

--- system-config-printer.py
+++ system-config-printer.py
@@ -3,8 +3,8 @@
 ## system-config-printer
 
 ## Copyright (C) 2006, 2007 Red Hat, Inc.
-## Copyright (C) 2006 Florian Festi <ffesti@redhat.com>
-## Copyright (C) 2006 Tim Waugh <twaugh@redhat.com>
+## Copyright (C) 2006, 2007 Florian Festi <ffesti@redhat.com>
+## Copyright (C) 2006, 2007 Tim Waugh <twaugh@redhat.com>
 
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
