611e0ab00e16ca284ea484a5288bb16bd7e747e9
twaugh <twaugh@8aafa1e6-caf3-4c12-8b34-7de0f4e7b767>
2008-04-15 16:09:28 +0000

    Merged trunk@2331
    git-svn-id: svn+ssh://fedorahosted.org/svn/system-config-printer/branches/0.7.74.x@2333 8aafa1e6-caf3-4c12-8b34-7de0f4e7b767

--- system-config-printer.py
+++ system-config-printer.py
@@ -1424,7 +1424,9 @@ class GUI:
                 if option not in self.server_side_options:
                     printer.unsetOption(option)
             for option in self.server_side_options.itervalues():
-                if option.is_changed() or saveall:
+                if (option.is_changed() or
+                    saveall and
+                    option.get_current_value () != option.system_default):
                     printer.setOption(option.name, option.get_current_value())
 
         except cups.IPPError, (e, s):
