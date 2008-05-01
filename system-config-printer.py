36abe5a923b7f554796d4158db42f5640762cac9
twaugh <twaugh@8aafa1e6-caf3-4c12-8b34-7de0f4e7b767>
2008-05-01 15:39:56 +0000

    2008-05-01  Tim Waugh  <twaugh@redhat.com>
            * my-default-printer.py: Guard against locale errors (bug
            #417951).
            * openprinting.py (OpenPrinting.__init__): Likewise.
            * system-config-printer.py (show_help): Likewise.
    
    git-svn-id: svn+ssh://fedorahosted.org/svn/system-config-printer/trunk@2383 8aafa1e6-caf3-4c12-8b34-7de0f4e7b767

--- system-config-printer.py
+++ system-config-printer.py
@@ -72,7 +72,11 @@ from smburi import SMBURI
 
 domain='system-config-printer'
 import locale
-locale.setlocale (locale.LC_ALL, "")
+try:
+    locale.setlocale (locale.LC_ALL, "")
+except locale.Error:
+    os.environ['LC_ALL'] = 'C'
+    locale.setlocale (locale.LC_ALL, "")
 from gettext import gettext as _
 import gettext
 gettext.textdomain (domain)
@@ -142,8 +146,15 @@ class GUI(GtkGUI, monitor.Watcher):
 
     def __init__(self, start_printer = None, change_ppd = False):
 
-        self.language = locale.getlocale(locale.LC_MESSAGES)
-        self.encoding = locale.getlocale(locale.LC_CTYPE)
+        try:
+            self.language = locale.getlocale(locale.LC_MESSAGES)
+            self.encoding = locale.getlocale(locale.LC_CTYPE)
+        except:
+            nonfatalException()
+            os.environ['LC_ALL'] = 'C'
+            locale.setlocale (locale.LC_ALL, "")
+            self.language = locale.getlocale(locale.LC_MESSAGES)
+            self.encoding = locale.getlocale(locale.LC_CTYPE)
         
         self.printer = None
         self.conflicts = set() # of options
