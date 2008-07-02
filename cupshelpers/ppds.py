85ee13c1b81f202868537130f45ab017774abd2a
Till Kamppeter <till@till-laptop.(none)>
2008-07-02 23:05:37 +0200

    "from .cupshelpers import ..." -> "from cupshelpers import ...". The dot breaks byte-compiling hich is standard in Debian's Python library packages.

--- cupshelpers/ppds.py
+++ cupshelpers/ppds.py
@@ -21,7 +21,7 @@
 ## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 import cups
-from .cupshelpers import parseDeviceID
+from cupshelpers import parseDeviceID
 import string
 import locale
 import os.path
