OpenPrinting/system-config-printer

commit c35de685c75542f12c7f539e77adc5e7694c1fc1

Avoid leaking SMB authentication dialog.
	* pysmb.py (AuthContext.perform_authentication): Destroy this
	dialog instead of hiding it.

pysmb.py
@@ -117,7 +117,7 @@ class AuthContext:
         username_entry.set_text (self.use_user)
         domain_entry.set_text (self.use_workgroup)
         response = d.run ()
-        d.hide ()
+        d.destroy ()
 
         if response == gtk.RESPONSE_CANCEL:
             self.cancel = True

commit 76bc28352d3d196699f5750c1fa0dfbfc9f0358d

Fixed SMB authentication (bug #464003).
	* pysmb.py (AuthContext.perform_authentication): Don't destroy
	authentication dialog until after we've fetched the details (bug
	#464003).

This was broken by commit c35de685c75542f12c7f539e77adc5e7694c1fc1.

pysmb.py
@@ -133,7 +133,6 @@ class AuthContext:
         username_entry.set_text (self.use_user)
         domain_entry.set_text (self.use_workgroup)
         response = d.run ()
-        d.destroy ()
 
         if response == gtk.RESPONSE_CANCEL:
             self.cancel = True
@@ -142,6 +141,7 @@ class AuthContext:
         self.use_user = username_entry.get_text ()
         self.use_password = password_entry.get_text ()
         self.use_workgroup = domain_entry.get_text ()
+        d.destroy ()
         return 1
 
     def initial_authentication (self):