0872261db29df905006d993abd40393583a03209
Tim Waugh <twaugh@redhat.com>
2008-12-11 17:17:44 +0000

    troubleshoot: reconnect after adjusting server settings.
    	* troubleshoot/ErrorLogCheckpoint.py
    	(ErrorLogCheckpoint.enable_clicked): Reconnect after adjusting
    	server settings.
    	* troubleshoot/ErrorLogFetch.py (ErrorLogFetch.button_clicked):
    	Likewise.

--- troubleshoot/ErrorLogCheckpoint.py
+++ troubleshoot/ErrorLogCheckpoint.py
@@ -22,6 +22,7 @@
 import cups
 import os
 import tempfile
+import time
 from base import *
 class ErrorLogCheckpoint(Question):
     def __init__ (self, troubleshooter):
@@ -168,6 +169,17 @@ class ErrorLogCheckpoint(Question):
                 debugprint ("Settings to set: " + repr (settings))
                 c.adminSetServerSettings (settings)
                 success = True
+
+                # Now reconnect.
+                attempt = 1
+                while attempt <= 5:
+                    try:
+                        time.sleep (1)
+                        c._connect ()
+                        break
+                    except RuntimeError:
+                        # Connection failed
+                        attempt += 1
             except cups.IPPError:
                 pass
             except RuntimeError:
