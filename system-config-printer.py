86f384671d5f79dd8b911849c59eef10151b22af
Tim Waugh <twaugh@redhat.com>
2008-12-12 18:03:07 +0000

    Provide localedir when binding textdomain for glade (trac #122).

--- system-config-printer.py
+++ system-config-printer.py
@@ -91,7 +91,7 @@ authconn.set_gettext_function (_)
 import gettext
 gettext.textdomain (domain)
 gettext.bindtextdomain (domain, config.localedir)
-gtk.glade.bindtextdomain (domain)
+gtk.glade.bindtextdomain (domain, config.localedir)
 pkgdata = config.pkgdatadir
 iconpath = os.path.join (pkgdata, 'icons/')
 sys.path.append (pkgdata)
