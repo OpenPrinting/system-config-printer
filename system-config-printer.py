OpenPrinting/system-config-printer

commit 8082e86658ff2cbfe6c7428223068b912811d64b

Attempt to fetch model information from network printers again.

system-config-printer.py
@@ -3167,7 +3167,9 @@ class NewPrinterGUI(GtkGUI):
             if page_nr == 1: # Device (first page)
                 self.auto_make, self.auto_model = None, None
                 self.device.uri = self.getDeviceURI()
-                if self.device.type == "hp":
+                if self.device.type in ["socket", "lpd", "ipp"]:
+                    self.getNetworkPrinterMakeModel ()
+                elif self.device.type == "hp":
                     faxuri = None
                     if host:
                         faxuri = self.get_hplip_uri_for_network_printer(host,
@@ -3656,7 +3658,8 @@ class NewPrinterGUI(GtkGUI):
         uri = stdout.strip ()
         return uri
 
-    def getNetworkPrinterMakeModel(self, device):
+    def getNetworkPrinterMakeModel(self):
+        device = self.device
         # Determine host name/IP
         host = None
         s = device.uri.find ("://")
@@ -3701,27 +3704,6 @@ class NewPrinterGUI(GtkGUI):
             (mk, md) = cupshelpers.ppds.ppdMakeModelSplit (make_and_model)
             device.id = "MFG:" + mk + ";MDL:" + md + ";DES:" + mk + " " + md + ";"
             device.id_dict = cupshelpers.parseDeviceID (device.id)
-        # Check whether the device is supported by HPLIP and replace
-        # its URI by an HPLIP URI
-        if host:
-            hplipuri = self.get_hplip_uri_for_network_printer(host, "print")
-            if hplipuri:
-                device.uri = hplipuri
-                s = hplipuri.find ("/usb/")
-                if s == -1: s = hplipuri.find ("/par/")
-                if s == -1: s = hplipuri.find ("/net/")
-                if s != -1:
-                    s += 5
-                    e = hplipuri[s:].find ("?")
-                    if e == -1: e = len (hplipuri)
-                    mdl = hplipuri[s:s+e].replace ("_", " ")
-                    if mdl.startswith ("hp ") or mdl.startswith ("HP "):
-                        mdl = mdl[3:]
-                        device.make_and_model = "HP " + mdl
-                        device.id = "MFG:HP;MDL:" + mdl + ";DES:HP " + mdl + ";"
-                        device.id_dict = cupshelpers.parseDeviceID (device.id)
-        # Return the host name/IP for further actions
-        return host
 
     def fillDeviceTab(self, current_uri=None):
         try:

commit a00e9b89ba5a1cdf561ed212cbc535c16b9d2e70

Prevent traceback when adding a printer driven by HPLIP (bug #477107).
	* system-config-printer.py (NewPrinterGUI.nextNPTab): Prevent
	traceback when adding a printer driven by HPLIP (bug #477107).

Problem was introduced in 8082e86658ff2cbfe6c7428223068b912811d64b.

system-config-printer.py
@@ -3186,8 +3186,7 @@ class NewPrinterGUI(GtkGUI):
                 self.auto_make, self.auto_model = None, None
                 self.device.uri = self.getDeviceURI()
                 if self.device.type in ["socket", "lpd", "ipp"]:
-                    self.getNetworkPrinterMakeModel ()
-                elif self.device.type == "hp":
+                    host = self.getNetworkPrinterMakeModel ()
                     faxuri = None
                     if host:
                         faxuri = self.get_hplip_uri_for_network_printer(host,
@@ -3677,6 +3676,12 @@ class NewPrinterGUI(GtkGUI):
         return uri
 
     def getNetworkPrinterMakeModel(self):
+        """
+        Try to determine the make and model for the currently selected
+        network printer, and store this in the data structure for the
+        printer.
+        Returns the hostname or None.
+        """
         device = self.device
         # Determine host name/IP
         host = None
@@ -3722,6 +3727,8 @@ class NewPrinterGUI(GtkGUI):
             (mk, md) = cupshelpers.ppds.ppdMakeModelSplit (make_and_model)
             device.id = "MFG:" + mk + ";MDL:" + md + ";DES:" + mk + " " + md + ";"
             device.id_dict = cupshelpers.parseDeviceID (device.id)
+
+        return host
 
     def fillDeviceTab(self, current_uri=None):
         try: