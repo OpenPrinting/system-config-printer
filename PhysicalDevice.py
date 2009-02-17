c2d61ca7ad008566c145172bdc664abef27d4fbd
Till Kamppeter <till.kamppeter@gmail.com>
2009-02-18 00:45:57 +0100

    Some device IDs lack MFG but have MDL, so consider a device as a backend only if both MFG and MDL are missing.

--- PhysicalDevice.py
+++ PhysicalDevice.py
@@ -29,10 +29,8 @@ class PhysicalDevice:
     def _canonical_id (self, device):
         mfg = device.id_dict.get ('MFG', '')
         mdl = device.id_dict.get ('MDL', '')
-        if mfg == '':
-            return ('', '')
 
-        if mdl.lower ().startswith (mfg.lower ()):
+        if mfg == '' or mdl.lower ().startswith (mfg.lower ()):
             make_and_model = mdl
         else:
             make_and_model = "%s %s" % (mfg, mdl)
@@ -105,23 +103,26 @@ class PhysicalDevice:
         if other == None or type (other) != type (self):
             return 1
 
-        if other.mfg == '' or self.mfg == '':
+        if (other.mfg == '' and other.mdl == '') or \
+           (self.mfg == '' and self.mdl == ''):
             # One or other is just a backend, not a real physical device.
-            if other.mfg == '' and self.mfg == '':
+            if other.mfg == '' and other.mdl == '' and \
+               self.mfg == '' and self.mdl == '':
                 return cmp (self.devices[0], other.devices[0])
 
-            if other.mfg == '':
+            if other.mfg == '' and other.mdl == '':
                 return -1
             return 1
 
-        if self.mdl.lower ().startswith (self.mfg.lower ()):
+        if self.mfg == '' or self.mdl.lower ().startswith (self.mfg.lower ()):
             our_make_and_model = self.mdl
         else:
             our_make_and_model = "%s %s" % (self.mfg, self.mdl)
         (our_mfg, our_mdl) = \
             cupshelpers.ppds.ppdMakeModelSplit (our_make_and_model)
 
-        if other.mdl.lower ().startswith (other.mfg.lower ()):
+        if other.mfg == '' or \
+                other.mdl.lower ().startswith (other.mfg.lower ()):
             other_make_and_model = other.mdl
         else:
             other_make_and_model = "%s %s" % (other.mfg, other.mdl)
