65091a1373899670708a036ef30f4697e8ecbc99
Till Kamppeter <till.kamppeter@gmail.com>
2009-02-17 22:47:24 +0100

    Unfortunately, the "usb"and "hp" CUPS backends sometimes report different device URIs for the same device. Therefore normalize make and model name with cupshelpers.ppds.ppdMakeModelSplit() for comparing. Also consider no serial number as matching any serial number.

--- PhysicalDevice.py
+++ PhysicalDevice.py
@@ -65,7 +65,7 @@ class PhysicalDevice:
             self.mdl = nicest (self.mdl, mdl)
 
             sn = device.id_dict.get ('SN', '')
-            if sn != self.sn:
+            if sn != '' and self.sn != '' and sn != self.sn:
                 raise RuntimeError
 
         self.devices.append (device)
@@ -114,12 +114,27 @@ class PhysicalDevice:
                 return -1
             return 1
 
-        mfgcmp = cmp (self.mfg_lower, other.mfg_lower)
+        if self.mdl.lower ().startswith (self.mfg.lower ()):
+            our_make_and_model = self.mdl
+        else:
+            our_make_and_model = "%s %s" % (self.mfg, self.mdl)
+        (our_mfg, our_mdl) = \
+            cupshelpers.ppds.ppdMakeModelSplit (our_make_and_model)
+
+        if other.mdl.lower ().startswith (other.mfg.lower ()):
+            other_make_and_model = other.mdl
+        else:
+            other_make_and_model = "%s %s" % (other.mfg, other.mdl)
+        (other_mfg, other_mdl) = \
+            cupshelpers.ppds.ppdMakeModelSplit (other_make_and_model)
+
+        mfgcmp = cmp (our_mfg.lower (), other_mfg.lower ())
         if mfgcmp != 0:
             return mfgcmp
-        mdlcmp = cmp (self.mdl_lower, other.mdl_lower)
+        mdlcmp = cmp (our_mdl.lower (), other_mdl.lower ())
         if mdlcmp != 0:
             return mdlcmp
+        if self.sn == '' or other.sn == '': return 0;
         return cmp (self.sn, other.sn)
 
 if __name__ == '__main__':
