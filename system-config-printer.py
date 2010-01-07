OpenPrinting/system-config-printer

commit 8db7f55c52c55dc3d1d7ed2ce1cdf2656e98140e

Make sure the printer we added still exists before selecting it (bug #551436).

system-config-printer.py
@@ -6897,6 +6897,8 @@ class NewPrinterGUI(GtkGUI):
                     self.mainapp.cups.addPrinter(name, ppd=ppd)
                 except cups.IPPError, (e, msg):
                     self.show_IPP_Error(e, msg)
+                    self.mainapp.cups._end_operation ()
+                    return
 
             self.mainapp.cups._end_operation ()
 
@@ -6923,6 +6925,11 @@ class NewPrinterGUI(GtkGUI):
                 break
 
             iter = model.iter_next (iter)
+
+        if not self.printers.has_key (name):
+            # At this stage the printer has disappeared even though we
+            # only added it moments ago.
+            return
 
         # Load information about the printer,
         # e.g. self.mainapp.server_side_options and self.mainapp.ppd

commit a523aa7d2ca90ddca6e67d49006af32566c868f9

Fixed typo introduced in 8db7f55 (bug #551436).

system-config-printer.py
@@ -6935,7 +6935,7 @@ class NewPrinterGUI(GtkGUI):
 
             iter = model.iter_next (iter)
 
-        if not self.printers.has_key (name):
+        if not self.mainapp.printers.has_key (name):
             # At this stage the printer has disappeared even though we
             # only added it moments ago.
             return