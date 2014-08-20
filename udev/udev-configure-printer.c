d450f74774446b108c852cf4f6ab81619cc47a8f
Tim Waugh <twaugh@redhat.com>
2014-08-20 12:16:11 +0100

    Fixed typo causing bluetooth queues not to be removed.

--- udev/udev-configure-printer.c
+++ udev/udev-configure-printer.c
@@ -1780,7 +1780,7 @@ do_remove (const char *devaddr)
       char *device_uri;
 
       device_uri = uri_from_bdaddr (devaddr);
-      remove_queue (devpath);
+      remove_queue (device_uri);
       g_free (device_uri);
       return 0;
     }
