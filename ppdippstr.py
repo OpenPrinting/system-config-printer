fdff67763556cd7cabdccf477e71b474368a3eec
lafricain79 <lafricain79@gmail.com>
2017-12-07 16:16:16 +0100

    New strings added

--- ppdippstr.py
+++ ppdippstr.py
@@ -92,18 +92,47 @@ def init ():
     global ppd
     ppd = TranslationDict ({
             "General": _("General"),
+           
+           #HP
             "Print Quality": _("Print Quality"),
+            	"Automatic": _("Automatic"),
+           
             "Printing Quality": _("Printing Quality"),
-            "Draft": _("Draft"),
-            "Normal": _("Normal"),
-            "Resolution": _("Resolution"),
-            "Color Mode": _("Color Mode"),
+		"Draft": _("Draft"),
+		"Normal": _("Normal"),
+		"Normal Color": _("Normal Color"),
+		"Normal Grayscale": _("Normal Grayscale"),	
+		"Draft Color": _("Draft Color"),
+		"Draft Grayscale": _("Draft Grayscale"),
+		"Best": _("Best"),
+		"High-Resolution Photo": _("High-Resolution Photo"),
+		"Fast Draft": _("Fast Draft"),
+
+	    "Installed Cartridges": _("Installed Cartridges"),
+		"Black Only": _("Fast Only"),
+		"TriColor Only": _("Fast Only"),		   	   	"Photo Only": _("Photo Only"),
+		"Black and TriColor": _("Black and TriColor"),
+		"Photo and TriColor": _("Photo and TriColor"),
+		   
+	    "Resolution": _("Resolution"),
+			   
+	    "Color Mode": _("Color Mode"),
+	    	"Hight Quality Grayscale": _("Hight Quality Grayscale"),
+	    	"Black Only Grayscale": _("Black Only Grayscale"),
+	    	
             "Quality": _("Quality"),
+            	"High Resolution": _("High Resolution"),
             "Paper Source": _("Paper source"),
                 "Auto Source": _("Auto Source"),
                 "Manual Feed": _("Manual Feed"),
                 "Middle Tray": _("Middle Tray"),
-                
+                "Upper or Only One InputSlot": _("Upper or Only One InputSlot"),
+                "Multi-purpose Tray": _("Multi-purpose Tray"),
+                "Drawer 1": _("Drawer 1"),
+                "Drawer 2": _("Drawer 2"),
+                "Tray 1": _("Tray 1"),
+                "Auto Select": _("Auto Select"),
+                    
             "Media Type": _("Media Type"),
                 "Bond": _("Bond"),
                 "Color": _("Color"),
@@ -113,7 +142,12 @@ def init ():
                 "Heavy": _("Heavy"),
                 "Light": _("Light"),
                 "Recycled": _("Recycled"),
-                
+                "Transparency": _("Transparency"),
+                "Plain Paper": _("Plain Paper"),
+                "Plain": _("Plain"),
+                "Photo Paper)": _("Photo Paper)"),
+                "Transparency Film": _("Transparency Film"),             
+                "CD or DVD Media": _("CD or DVD Media"),
             "Print Density": _("Print Density"),
             	"Extra Light(1)": _("Extra Light(1)"),
             	"Light (2)": _("Light (2)"),
@@ -124,25 +158,111 @@ def init ():
             
             "Adjustment": _("Adjustment"),
             "Halftone Algorithm": _("Halftone Algorithm")
-           	 "Default":_("Default")
+           	 "Default":_("Default"),
             
-            "Miscellaneous": _("Miscellaneous")
+            "Miscellaneous": _("Miscellaneous"),
+           	"N-up Orientation": _("N-up Orientation"),
+            	"N-up Printing": _("N-up Printing"),
+         	"Lanscape": _("Lanscape"),
+         	"Seascape": _("Seascape"),
+            "Media Size": _("Media Size"),
+            "Output Mode": _("Output Mode"),
+                 "Grayscale": _("Grayscale"),
             
+            #Brother      
+            "Two-Sided": _("Two-Sided"),
+            "Print Settings": _("Print Settings"),
+            "Print Settings (Advanced)": _("Print Settings  (Advanced)"),
+            "Color Settings": _("Color Settings"),
+            "Color Settings (Advanced)": _("Color Settings (Advanced)"),                     
+	     	"Brightness": _("Brightness"),
+		"Contrast": _("Contrast"),
+		"Red": _("Red"),            
+		"Green": _("Green"),            
+		"Blue": _("Blue"),
+                      
             #Epson xp serie
             "_Media Size": _("_Media Size"),
             "_Grayscale": _("_Grayscale"),
             "_Brightness": _("_Brightness"),
             "_Contrast": _("_Contrast"),
             "_Saturation": _("_Saturation"),
-            "Off": _("Off"),
-            "On": _("On"),   
+		"On": _("On"),
+            # Options 
+            "Installable Options": _("Installable Options"),
+            "Duplexer Installed": _("Duplexer Installed"),
+            
+            # Canon
+            "Color Model": _("Color Model"),
+            "Color Precision": _("Color Precision"),
+	    "Resolution ": _("Resolution "),
+            "Printer Features Common": _("Printer Features Common"),
+            	"CD Hub Size": _("CD Hub Size"),
+            	"Ink Type": _("Ink Type"),
+            "Toner Save ": _("Toner Save "),
+            	"ON": _("ON"),      	
+            "Toner Density": _("Toner Density"),
+            "Media Type ": _("Media Type "),
+            "Collate ": _("Collate "),
+            "Image Refinement ": _("Image Refinement "),
+            "Halftones ": _("Halftones "),
+            "Duplex": _("Duplex"),
+            	"OFF": _("OFF"),
+            	"ON (Long-edged Binding)": _("ON (Long-edged Binding)"),
+            	"ON (Short-edged Binding)": _("ON (Short-edged Binding)"),
+            #Samsung
+            "Paper Size": _("Paper Size"),
+            "Paper Type": _("Paper Type"),
+            	"Thin": _("Thin"),
+            	"Thick": _("Thick"),
+            	"Thicker": _("Thicker") ,     	
+            "Edge Enhance": _("Edge Enhance"),
+            "Skip Blank Pages": _("Skip Blank Pages"),
+            "Double-sided Printing": _("Double-sided Printing"),
+      	  	"None": _("None"),
+            "Reverse Duplex Printing": _("Reverse Duplex Printing"),
+            	"Long Edge": _("Long Edge"),
+            	"Short Edge": _("Short Edge"),
+            "Two-sided": _("Two-sided"),
+            	"Long Edge": _("Long Edge"),
+            	"Short Edge": _("Short Edge"),
+      
+            #Ricoh
+            "Finisher": _("Finisher"),
+            "Option Tray": _("Option Tray"),
+            "External Tray": _("External Tray"),
+            "Internal Tray": _("Internal Tray"),
+            "Internal Tray 2": _("Internal Tray 2"),
+            "Internal Shift Tray": _("Internal Shift Tray"),
+            	"Not Installed": _("Not Installed"),
+            	"Installed": _("Installed"),
+            "PageSize": _("PageSize"),
+            "InputSlot": _("InputSlot"),
+                "Tray 2": _("Tray 2"),
+                "Tray 3": _("Tray 3"),
+            	"Tray 4": _("Tray 4"),                    
+                "Bypass Tray": _("Bypass Tray"),        
+            "Destination": _("Destination"),
+            "Staple": _("Staple"),                
+            "Punch": _("Punch"),
+            "Toner Saving": _("Toner Saving"),
+            "Gradation": _("Gradation"),
+            "Fast": _("Fast"),
             
             # HPIJS options
             "Printout Mode": _("Printout mode"),
             "Draft (auto-detect paper type)":
                 _("Draft (auto-detect-paper type)"),
+            "Draft (Color cartridge)":
+                _("Draft (Color cartridge)"),                
+            "Draft Grayscale (Black cartridge)":
+                _("Draft grayscale (Black cartridge)"),
             "Draft Grayscale (auto-detect paper type)":
                 _("Draft grayscale (auto-detect-paper type)"),
+            "Normal (Color cartridge)":
+                _("Normal (Color cartridge)"),
+            "Normal Grayscale (Black cartridge)":
+                _("Normal grayscale (Black cartridge)"),
             "Normal (auto-detect paper type)":
                 _("Normal (auto-detect-paper type)"),
             "Normal Grayscale (auto-detect paper type)":
@@ -151,7 +271,12 @@ def init ():
                 _("High quality (auto-detect-paper type)"),
             "High Quality Grayscale (auto-detect paper type)":
                 _("High quality grayscale (auto-detect-paper type)"),
+ 	    "High Quality (Color cartridge)":
+                _("High quality (Color cartridge)"),
+            "High Quality Grayscale (Black cartridge)":
+                _("High quality grayscale (Black cartridge)"),
             "Photo (on photo paper)": _("Photo (on photo paper)"),
+            "Photo (Color cartridge, on photo paper)": _("Photo (Color cartridge, on photo paper)"),
             "Best Quality (color on photo paper)":
                 _("Best quality (color on photo paper)"),
             "Normal Quality (color on photo paper)":
@@ -188,10 +313,18 @@ def init ():
             "Resolution, Quality, Ink Type, Media Type":
                 _("Resolution, quality, ink type, media type"),
             "Controlled by 'Printout Mode'": _("Controlled by 'Printout mode'"),
+            "300 dpi, Color, Color Cartr.":
+                _("300 dpi, Color, Color Cartr."),
             "300 dpi, Color, Black + Color Cartr.":
                 _("300 dpi, color, black + color cartridge"),
+            "300 dpi, Draft, Color, Color Cartr.":
+                _("300 dpi, Draft, Color, Color Cartr."),
             "300 dpi, Draft, Color, Black + Color Cartr.":
                 _("300 dpi, draft, color, black + color cartridge"),
+            "300 dpi, Draft, Grayscale, Black Cartr.":
+                _("300 dpi, Draft, Grayscale, Black Cartr."),
+            "300 dpi, Grayscale, Black Cartr.":
+                _("300 dpi, Grayscale, Black Cartr."),
             "300 dpi, Draft, Grayscale, Black + Color Cartr.":
                 _("300 dpi, draft, grayscale, black + color cartridge"),
             "300 dpi, Grayscale, Black + Color Cartr.":
