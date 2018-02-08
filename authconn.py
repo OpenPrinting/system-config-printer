OpenPrinting/system-config-printer

commit 47973c801f10a3b2575bc45a390cadc1324b787f

Remove deprecated Gtk objects

authconn.py
@@ -67,23 +67,27 @@ class AuthDialog(Gtk.Dialog):
         vbox.pack_start (self.prompt_label, False, False, 0)
 
         num_fields = len (auth_info_required)
-        table = Gtk.Table (n_rows=num_fields, n_columns=2)
-        table.set_row_spacings (6)
-        table.set_col_spacings (6)
+        grid = Gtk.Grid()
+        grid.insert_row(num_fields)
+        grid.insert_column(2)
+        grid.set_row_spacing (6)
+        grid.set_column_spacing (6)
 
         self.field_entry = []
         for i in range (num_fields):
             field = auth_info_required[i]
             label = Gtk.Label (label=_(self.AUTH_FIELD.get (field, field)))
             label.set_alignment (0, 0.5)
-            table.attach (label, 0, 1, i, i + 1)
+            grid.attach (label, 0, 1, i, i + 1)
+            grid.attach (label, 0, i, 1, 1)
             entry = Gtk.Entry ()
             entry.set_visibility (field != 'password')
-            table.attach (entry, 1, 2, i, i + 1, 0, 0)
+            grid.attach (entry, 1, 2, i, i + 1, 0, 0)
+            grid.attach (entry, 1, i, 1, 1)
             self.field_entry.append (entry)
 
         self.field_entry[num_fields - 1].set_activates_default (True)
-        vbox.pack_start (table, False, False, 0)
+        vbox.pack_start (grid, False, False, 0)
         hbox.pack_start (vbox, False, False, 0)
         self.vbox.pack_start (hbox, False, False, 0)
 

newprinter.py
@@ -4262,29 +4262,28 @@ class NewPrinterGUI(GtkGUI):
             if group.name != "InstallableOptions":
                 continue
             self.installable_options = True
-            table = Gtk.Table(n_rows=1, n_columns=3, homogeneous=False)
-            table.set_col_spacings(6)
-            table.set_row_spacings(6)
-            container.add(table)
+            grid = Gtk.Grid()
+            grid.set_column_spacing(6)
+            grid.set_row_spacing(6)
+            container.add(grid)
             rows = 0
 
             for nr, option in enumerate(group.options):
                 if option.keyword == "PageRegion":
                     continue
                 rows += 1
-                table.resize (rows, 3)
                 o = OptionWidget(option, self.ppd, self)
-                table.attach(o.conflictIcon, 0, 1, nr, nr+1, 0, 0, 0, 0)
+                grid.attach(o.conflictIcon, 0, nr, 1, 1)
 
-                hbox = Gtk.HBox()
+                hbox = Gtk.Box()
                 if o.label:
                     a = Gtk.Alignment.new (0.5, 0.5, 1.0, 1.0)
                     a.set_padding (0, 0, 0, 6)
                     a.add (o.label)
-                    table.attach(a, 1, 2, nr, nr+1, Gtk.AttachOptions.FILL, 0, 0, 0)
-                    table.attach(hbox, 2, 3, nr, nr+1, Gtk.AttachOptions.FILL, 0, 0, 0)
+                    grid.attach(a, 1, nr, 1, 1)
+                    grid.attach(hbox, 2, nr, 1, 1)
                 else:
-                    table.attach(hbox, 1, 3, nr, nr+1, Gtk.AttachOptions.FILL, 0, 0, 0)
+                    grid.attach(hbox, 1, nr, 2, 1)
                 hbox.pack_start(o.selector, False, False, 0)
                 self.options[option.keyword] = o
         if not self.installable_options:

printerproperties.py
@@ -809,19 +809,14 @@ class PrinterPropertiesDialog(GtkGUI):
             self.tblJOOther.hide()
             return
 
-        self.tblJOOther.resize (n, 3)
         children = self.tblJOOther.get_children ()
         for child in children:
             self.tblJOOther.remove (child)
         i = 0
         for opt in self.other_job_options:
-            self.tblJOOther.attach (opt.label, 0, 1, i, i + 1,
-                                    xoptions=Gtk.AttachOptions.FILL,
-                                    yoptions=Gtk.AttachOptions.FILL)
+            self.tblJOOther.attach (opt.label, 0, i, 1, 1)
             opt.label.set_alignment (0.0, 0.5)
-            self.tblJOOther.attach (opt.selector, 1, 2, i, i + 1,
-                                    xoptions=Gtk.AttachOptions.FILL,
-                                    yoptions=0)
+            self.tblJOOther.attach (opt.selector, 1, i, 1, 1)
             opt.selector.set_sensitive (editable)
 
             btn = Gtk.Button.new_from_icon_name (Gtk.STOCK_REMOVE,
@@ -829,9 +824,7 @@ class PrinterPropertiesDialog(GtkGUI):
             btn.connect("clicked", self.on_btnJOOtherRemove_clicked)
             btn.pyobject = opt
             btn.set_sensitive (editable)
-            self.tblJOOther.attach(btn, 2, 3, i, i + 1,
-                                   xoptions=0,
-                                   yoptions=0)
+            self.tblJOOther.attach(btn, 2, i, 1, 1)
             i += 1
 
         self.tblJOOther.show_all ()
@@ -1567,12 +1560,12 @@ class PrinterPropertiesDialog(GtkGUI):
             rows = 1 + (cols - 1) / 4
             if cols > 4:
                 cols = 4
-            table = Gtk.Table (n_rows=round(rows),
-                               n_columns=cols,
-                               homogeneous=True)
-            table.set_col_spacings (6)
-            table.set_row_spacings (12)
-            self.vboxMarkerLevels.pack_start (table, False, False, 0)
+            grid = Gtk.Grid()
+            grid.set_column_homogeneous(True)
+            grid.set_row_homogeneous(True)
+            grid.set_column_spacing (6)
+            grid.set_row_spacing (12)
+            self.vboxMarkerLevels.pack_start (grid, False, False, 0)
             for color, name, marker_type, level in markers:
                 if name is None:
                     name = ''
@@ -1584,8 +1577,8 @@ class PrinterPropertiesDialog(GtkGUI):
                 row = num_markers / 4
                 col = num_markers % 4
 
-                vbox = Gtk.VBox (spacing=6)
-                subhbox = Gtk.HBox ()
+                vbox = Gtk.Box (spacing=6)
+                subhbox = Gtk.Box ()
                 inklevel = gtkinklevel.GtkInkLevel (color, level)
                 inklevel.set_tooltip_text ("%d%%" % level)
                 subhbox.pack_start (inklevel, True, False, 0)
@@ -1594,7 +1587,7 @@ class PrinterPropertiesDialog(GtkGUI):
                 label.set_width_chars (10)
                 label.set_line_wrap (True)
                 vbox.pack_start (label, False, False, 0)
-                table.attach (vbox, col, col + 1, row, row + 1)
+                grid.attach (vbox, col, row, 1, 1)
                 num_markers += 1
 
         self.vboxMarkerLevels.show_all ()
@@ -1747,10 +1740,10 @@ class PrinterPropertiesDialog(GtkGUI):
                 frame.add (container)
                 tab_label = self.lblPOptions
 
-            table = Gtk.Table(n_rows=1, n_columns=3, homogeneous=False)
-            table.set_col_spacings(6)
-            table.set_row_spacings(6)
-            container.add(table)
+            grid = Gtk.Grid()
+            grid.set_column_spacing(6)
+            grid.set_row_spacing(6)
+            container.add(grid)
 
             rows = 0
 
@@ -1765,19 +1758,18 @@ class PrinterPropertiesDialog(GtkGUI):
                 if option.keyword == "PageRegion":
                     continue
                 rows += 1
-                table.resize (rows, 3)
                 o = OptionWidget(option, ppd, self, tab_label=tab_label)
-                table.attach(o.conflictIcon, 0, 1, nr, nr+1, 0, 0, 0, 0)
+                grid.attach(o.conflictIcon, 0, nr, 1, 1)
 
-                hbox = Gtk.HBox()
+                hbox = Gtk.Box()
                 if o.label:
                     a = Gtk.Alignment.new(0.5, 0.5, 1.0, 1.0)
                     a.set_padding (0, 0, 0, 6)
                     a.add (o.label)
-                    table.attach(a, 1, 2, nr, nr+1, Gtk.AttachOptions.FILL, 0, 0, 0)
-                    table.attach(hbox, 2, 3, nr, nr+1, Gtk.AttachOptions.FILL, 0, 0, 0)
+                    grid.attach(a, 1, nr, 1, 1)
+                    grid.attach(hbox, 2, nr, 1, 1)
                 else:
-                    table.attach(hbox, 1, 3, nr, nr+1, Gtk.AttachOptions.FILL, 0, 0, 0)
+                    grid.attach(hbox, 1, nr, 2, 1)
                 hbox.pack_start(o.selector, False, False, 0)
                 self.options[option.keyword] = o
                 o.selector.set_sensitive(editable)

pysmb.py
@@ -119,21 +119,21 @@ class AuthContext:
         label.set_line_wrap (True)
         vbox.pack_start (label, False, False, 0)
 
-        table = Gtk.Table (n_rows=3, n_columns=2)
-        table.set_row_spacings (6)
-        table.set_col_spacings (6)
-        table.attach (Gtk.Label(label=_("Username:")), 0, 1, 0, 1, 0, 0)
+        grid = Gtk.Grid()
+        grid.set_row_spacing (6)
+        grid.set_column_spacing (6)
+        grid.attach (Gtk.Label(label=_("Username:")), 0, 0, 1, 1)
         username_entry = Gtk.Entry ()
-        table.attach (username_entry, 1, 2, 0, 1, 0, 0)
-        table.attach (Gtk.Label(label=_("Domain:")), 0, 1, 1, 2, 0, 0)
+        grid.attach (username_entry, 1, 0, 1, 1)
+        grid.attach (Gtk.Label(label=_("Domain:")), 0, 1, 1, 1)
         domain_entry = Gtk.Entry ()
-        table.attach (domain_entry, 1, 2, 1, 2, 0, 0)
-        table.attach (Gtk.Label(label=_("Password:")), 0, 1, 2, 3, 0, 0)
+        grid.attach (domain_entry, 1, 1, 1, 1)
+        grid.attach (Gtk.Label(label=_("Password:")), 0, 2, 1, 1)
         password_entry = Gtk.Entry ()
         password_entry.set_activates_default (True)
         password_entry.set_visibility (False)
-        table.attach (password_entry, 1, 2, 2, 3, 0, 0)
-        vbox.pack_start (table, False, False, 0)
+        grid.attach (password_entry, 1, 2, 1, 1)
+        vbox.pack_start (grid, False, False, 0)
         hbox.pack_start (vbox, False, False, 0)
         d.vbox.pack_start (hbox, False, False, 0)
         self.dialog_shown = True

troubleshoot/Locale.py
@@ -33,24 +33,22 @@ class Locale(Question):
                                     "not the printer's default page size.  "
                                     "If this is not intentional it may cause "
                                     "alignment problems."))
-        table = Gtk.Table (n_rows=2, n_columns=2)
-        table.set_row_spacings (6)
-        table.set_col_spacings (6)
-        page.pack_start (table, False, False, 0)
+        grid = Gtk.Grid()
+        grid.set_row_spacing (6)
+        grid.set_column_spacing (6)
+        page.pack_start (grid, False, False, 0)
         self.printer_page_size = Gtk.Label ()
         self.printer_page_size.set_alignment (0, 0)
         self.job_page_size = Gtk.Label ()
         self.job_page_size.set_alignment (0, 0)
         label = Gtk.Label(label=_("Print job page size:"))
         label.set_alignment (0, 0)
-        table.attach (label, 0, 1, 0, 1, xoptions=Gtk.AttachOptions.FILL, yoptions=0)
-        table.attach (self.job_page_size, 1, 2, 0, 1,
-                      xoptions=Gtk.AttachOptions.FILL, yoptions=0)
+        grid.attach (label, 0, 0, 1, 1)
+        grid.attach (self.job_page_size, 1, 0, 1, 1)
         label = Gtk.Label(label=_("Printer page size:"))
         label.set_alignment (0, 0)
-        table.attach (label, 0, 1, 1, 2, xoptions=Gtk.AttachOptions.FILL, yoptions=0)
-        table.attach (self.printer_page_size, 1, 2, 1, 2,
-                      xoptions=Gtk.AttachOptions.FILL, yoptions=0)
+        grid.attach (label, 0, 1, 1, 1)
+        grid.attach (self.printer_page_size, 1, 1, 1, 1)
         troubleshooter.new_page (page, self)
 
     def display (self):

troubleshoot/RemoteAddress.py
@@ -29,24 +29,24 @@ class RemoteAddress(Question):
                                   _("Please enter as many details as you "
                                     "can about the network address of this "
                                     "printer."))
-        table = Gtk.Table (n_rows=2, n_columns=2)
-        table.set_row_spacings (6)
-        table.set_col_spacings (6)
-        page.pack_start (table, False, False, 0)
+        grid = Gtk.Grid()
+        grid.set_row_spacing (6)
+        grid.set_column_spacing (6)
+        page.pack_start (grid, False, False, 0)
 
         label = Gtk.Label(label=_("Server name:"))
         label.set_alignment (0, 0)
-        table.attach (label, 0, 1, 0, 1)
+        grid.attach (label, 0, 0, 1, 1)
         self.server_name = Gtk.Entry ()
         self.server_name.set_activates_default (True)
-        table.attach (self.server_name, 1, 2, 0, 1)
+        grid.attach (self.server_name, 1, 0, 1, 1)
 
         label = Gtk.Label(label=_("Server IP address:"))
         label.set_alignment (0, 0)
-        table.attach (label, 0, 1, 1, 2)
+        grid.attach (label, 0, 1, 1, 1)
         self.server_ipaddr = Gtk.Entry ()
         self.server_ipaddr.set_activates_default (True)
-        table.attach (self.server_ipaddr, 1, 2, 1, 2)
+        grid.attach (self.server_ipaddr, 1, 1, 1, 1)
 
         troubleshooter.new_page (page, self)
 

commit 2766d74a98e44693f5e966d541a6303b966c088b

Fix constructing the auth dialog
47973c80 (Remove deprecated Gtk objects) broke the auth dialog.

Fixes https://bugs.archlinux.org/task/57364.

authconn.py
@@ -78,11 +78,9 @@ class AuthDialog(Gtk.Dialog):
             field = auth_info_required[i]
             label = Gtk.Label (label=_(self.AUTH_FIELD.get (field, field)))
             label.set_alignment (0, 0.5)
-            grid.attach (label, 0, 1, i, i + 1)
             grid.attach (label, 0, i, 1, 1)
             entry = Gtk.Entry ()
             entry.set_visibility (field != 'password')
-            grid.attach (entry, 1, 2, i, i + 1, 0, 0)
             grid.attach (entry, 1, i, 1, 1)
             self.field_entry.append (entry)
 