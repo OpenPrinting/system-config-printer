69ca2505f40e2d91f2762c0534a567dc8e2da881
SinghCod3r <ayushsinghceee@gmail.com>
2026-09-12 13:22:54 +0530

    Disable Forward while driver search is pending

--- newprinter.py
+++ newprinter.py
@@ -852,6 +852,7 @@ class NewPrinterGUI(GtkGUI):
 
         self._getPPDs_reply (ppdsloader)
         if not self.ppds:
+            self.setNPButtons()
             return
 
         if ppdsloader._jockey_has_answered:
@@ -1209,6 +1210,7 @@ class NewPrinterGUI(GtkGUI):
 
         if not keep_going:
             debugprint ('Interrupting execution of nextNPTab(): Operations pending')
+            self.btnNPForward.set_sensitive (False)
             return
 
         order = self._getPagesOrderForDialogMode ()
